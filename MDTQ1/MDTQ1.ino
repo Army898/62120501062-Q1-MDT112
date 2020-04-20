0b6d2f7358a3b49da07aecc8010636694f86bdcb
Army898 <61343355+Army898@users.noreply.github.com>
2020-04-21 02:43:53 +0700

    95%

--- MDTQ1/MDTQ1.ino
+++ MDTQ1/MDTQ1.ino
@@ -16,25 +16,205 @@ void setup(){
     tone(8,400,100);
     Serial.begin(9600);
 }
+int count = 0 ;
+int light = 3;
 void loop()
-{
-    int light = 3; 
-    double count = 0;
-    for (light = 3 ; light <= 13 ; light++)
+{   
+    if (count == 0);
+    {            
+        for (light = 3 ; light <= 13 ; light++)
+        {
+            if(light == 8)
+            {
+                digitalWrite(light,0);
+                continue;
+            }
+            digitalWrite(light, 1);
+            delay(100);
+            digitalWrite(light, 0);
+            if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);  
+                    delay(500);     
+                }        
+        }
+        for (light = 13 ; light >= 3 ; light--)
+        {
+            if(light == 8)
+            {
+                digitalWrite(light,0);
+                continue;
+            }
+            digitalWrite(light, 1);
+            delay(100);
+            digitalWrite(light, 0);
+            if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }       
+        }
+    } 
+    if (count == 1)
     {
-        digitalWrite(light, 1);
-        delay(100);
-        digitalWrite(light, 0);        
-    }
-    for (light = 13 ; light >= 3 ; light--)
+        for (light = 3 ; light <= 13 ; light++)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                
+                digitalWrite(light, 1);
+                delay(50);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }        
+            }
+        for (light = 13 ; light >= 3 ; light--)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(50);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }
+            }
+        Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, count)) + ")");
+        tone(8,400,100);
+    }     
+    if (count == 2)
     {
-        digitalWrite(light, 1);
-        delay(100);
-        digitalWrite(light, 0);
-    }
-    if (digitalRead(2)==0){         
-        Serial.println("Faster X 2 (Current Speed : X" + String(count*2) + ")");
-        tone(8,400,100); 
-        count++;       
-    }    
-}
+        for (light = 3 ; light <= 13 ; light++)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(25);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }        
+            }
+        for (light = 13 ; light >= 3 ; light--)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(25);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {   
+                            
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }
+            }
+        Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, count)) + ")");
+        tone(8,400,100);
+    }     
+    if (count == 3)
+    {
+        for (light = 3 ; light <= 13 ; light++)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(12.5);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }        
+            }
+        for (light = 13 ; light >= 3 ; light--)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(12.5);
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }
+            }
+        Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, count)) + ")");
+        tone(8,400,100);
+    }     
+    if (count > 3)
+    {
+       int hran = count;
+        for (light = 3 ; light <= 13 ; light++)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(100 / pow(2,hran));
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }        
+            }
+        for (light = 13 ; light >= 3 ; light--)
+            {
+                if(light == 8)
+                {
+                    digitalWrite(light,0);
+                    continue;
+                }
+                digitalWrite(light, 1);
+                delay(100 / pow(2,hran));
+                digitalWrite(light, 0);
+                if (digitalRead(2)==0)
+                {           
+                    count++;
+                    Serial.println(count);
+                    delay(500);       
+                }
+            }
+        Serial.println("Faster X 2 (Current Speed : X" + String(pow(2, count)) + ")");
+        tone(8,400,100);
+    }     
+}  
+    
